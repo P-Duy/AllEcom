45b2b46f866371b899c856b9fc189ac2b1803867
PhamDuy <vanduy342001@gmail.com>
2023-12-18 15:23:33 +0700

    Logout Feature

--- userauths/views.py
+++ userauths/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.conf import settings
 
@@ -57,3 +57,9 @@ def login_view(request):
     context = {}
 
     return render(request, "userauths/sign_in.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You are logged out")
+    return redirect("userauths:sign-in")
